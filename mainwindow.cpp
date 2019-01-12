91baf96bab38ad14ce47bbaa16878a712529d54d
Liretta <Liretta26@gmail.com>
2019-01-12 11:59:44 +0200

    added picture in graphic and in code

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,20 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "QMessageBox"
+#include <QMessageBox>
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap pic(":/new/prefix1/pic/01.jpg");
+
+    //this code capture the image - NOT worked?
+    //int h = ui->image2->height();
+   // int w = ui->image2->width();
+   // ui->image2->setPixmap(pic.scaled(w,h,Qt::KeepAspectRatio));
+    ui->image2->setPixmap(pic);
 }
 
 MainWindow::~MainWindow()
@@ -20,8 +28,15 @@ void MainWindow::on_pushButton_clicked()
     QString password = ui->Pass->text();
 
     if(login == "Login" && password == "Password")
+       {
+         ui->statusBar->showMessage("Authorization succes!");
         QMessageBox::information(this, "Authorization success", "Success!");
-    else {
+
+       }
+    else
+    {
+         ui->statusBar->showMessage("Wrong login or password!");
         QMessageBox::warning(this, "Error!", "Wrong login or password");
+
     }
 }
